41e94a30d4245a21200b99211d48f0fa926e3803
hqtay <tayhuiqithq@gmail.com>
2014-11-01 14:01:30 -0700

    initial tiling code

---
+++ lava/Assets/Scripts/Tiling.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Tiling : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+public class TileManager {
+	int height;
+	int width;
+	List<Tile> tiles;
+
+	public TileManager(int height, int width) {
+		this.height = height;
+		this.width = width;
+		tiles = new List<Tile> ();
+	}
+}
+
+public class Tile {
+	Vector3 position;
+}
